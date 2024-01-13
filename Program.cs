838fcc01151bdfc0129c098841a26299024c2494
Julien Giguere <juliengiguere887@gmail.com>
2024-01-13 07:15:36 -0500

    dure de session

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddSession(options =>
 {
     options.Cookie.Name = ".AdventureWorks.Session";
-    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.IdleTimeout = TimeSpan.FromSeconds(3600);
     options.Cookie.IsEssential = true;
 });
 builder.Services.AddHttpContextAccessor();
