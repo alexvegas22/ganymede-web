b2e8221fa555622385e0480bd6ce2929fc26833c
mfatene <2023073@crosemont.qc.ca>
2024-01-13 04:38:55 -0500

    Dons et Joindre l'équipe Fonctionnelle

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,8 +32,12 @@ public class HomeController : Controller
     {
         return View();
     }
+	public IActionResult Dons()
+	{
+		return View();
+	}
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
